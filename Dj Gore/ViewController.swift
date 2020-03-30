f72007b7fb86e8055205b4b41cdf45924036b690
codecat15 <codecat15@gmail.com>
2020-03-29 21:25:10 -0500

    Removing the extension of UITextField
    - removed the extension of UITextField to use the custom password text field
    - updated the icons in the asset folder
    - code clean up from the viewcontroller

--- Dj Gore/ViewController.swift
+++ Dj Gore/ViewController.swift
@@ -9,58 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var userTF: UITextField!
+
+    @IBOutlet weak var userTF: PasswordTextField!
     
-   
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        userTF.setToolBar()
-        userTF.showHidePassword()
-        
-        
     }
 
     @objc func doneBttonClicked() {
         userTF.resignFirstResponder()
     }
-
-}
-
-
-
-extension UITextField {
-    
-    func setToolBar() {
-        let toolBar = UIToolbar()
-        toolBar.sizeToFit()
-        let done = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneBttonClicked))
-        toolBar.setItems([done], animated: true)
-        self.inputAccessoryView = toolBar
-    }
-    
-    @objc func doneBttonClicked() {
-        self.resignFirstResponder()
-    }
-    
-    func showHidePassword() {
-        let eye = UIButton(frame: CGRect(x: self.frame.width - 30, y: 0, width: 25, height: 30))
-        eye.tag = 0
-        eye.setBackgroundImage(UIImage(named: "closeEye"), for: .normal)
-        eye.addTarget(self, action: #selector(performAction(eye:)), for: .touchUpInside)
-        self.rightViewMode = .always
-        self.rightView = eye
-    }
-    
-    @objc func performAction(eye: UIButton) {
-        if eye.tag == 0 {
-            eye.tag = 1
-            self.isSecureTextEntry = true
-            eye.setBackgroundImage(UIImage(named: "openEye"), for: .normal)
-        }else {
-            eye.tag = 0
-            self.isSecureTextEntry = false
-            eye.setBackgroundImage(UIImage(named: "closeEye"), for: .normal)
-        }
-     }
 }
